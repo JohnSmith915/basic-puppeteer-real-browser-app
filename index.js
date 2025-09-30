d6234e912bcfd5e2b2743879f50f82a3fc939eaa
JohnSmith915 <johnsmithacctesttemp@gmail.com>
2025-09-30 17:21:58 +0800

    change port to 8080

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const app = express();
         }
     });
 
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 8080;
     // Start the server
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
